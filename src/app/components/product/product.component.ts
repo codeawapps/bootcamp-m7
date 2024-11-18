b89e7227e1daae0b63816c2c1568bc5819885bc7
adri <adrimonte994@gmail.com>
2024-11-18 16:54:24 +0100

    feat: example

--- src/app/components/product/product.component.ts
+++ src/app/components/product/product.component.ts
@@ -9,6 +9,26 @@ import { Product } from '../../models/product.interface';
   styleUrl: './product.component.scss'
 })
 export class ProductComponent {
-  @Input() product: Product | undefined;
+  @Input({ required: true }) product!: Product;
+
+  // 1. public
+  // 2. protected
+  // 3. private
+
+  protected onClickStock(isAdd: boolean = false): void {
+    isAdd
+      ? this.product.stock += 5
+      : this.reduceStock();
+  }
+
+  protected hasStock(): boolean {
+    return this.product.stock > 0;
+  }
+
+  private reduceStock(): void {
+    this.product.stock = this.product.stock <= 5
+      ? 0
+      : this.product.stock - 5;
+  }
 
 }
